be596a3a9d4cfa0e8ebe60eebb42b881858f4516
corentinperroux <corentin7301@gmail.com>
2020-12-11 16:21:06 +0100

    title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   ssr: false,
   target: 'static',
   head: {
-    title: 'VScode',
+    title: 'VScode TEAM',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
